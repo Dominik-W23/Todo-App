248d67f3f3866de754dbee61e6983983fe4954ef
Domin <domin0909@gmail.com>
2023-10-01 14:22:45 +0200

    Implement functioning Edit button

--- gui.py
+++ gui.py
@@ -3,10 +3,14 @@ import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Enter a todo", key='todo')
-add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='items',
+                      enable_events=True, size=(40, 15))
+
+add_button = sg.Button('Add')
+edit_button = sg.Button('Edit')
 
 app_window = sg.Window("To-do App",
-                       layout=[[label], [input_box, add_button]],
+                       layout=[[label], [input_box, add_button], [list_box, edit_button]],
                        font=('Helvetica', 20))
 
 while True:
@@ -16,7 +20,27 @@ while True:
         todos = functions.get_todos()
         todos.append(value['todo'] + '\n')
         functions.write_todos(todos)
+        app_window['items'].update(values=todos)
+        app_window['todo'].update(value='')
+
+    elif event == 'Edit':
+        todos = functions.get_todos()
+        todo_to_edit = value['items'][0]
+        todo_to_write = value['todo']
+
+        index_to_edit = todos.index(todo_to_edit)
+        todos[index_to_edit] = todo_to_write
+
+        functions.write_todos(todos)
+        app_window['items'].update(values=todos)
+
+    elif event == 'items':
+        app_window['todo'].update(value=value['items'][0])
+
     elif event == sg.WIN_CLOSED:
         break
+
+    print(event)
+    print(value)
 
 app_window.close()
